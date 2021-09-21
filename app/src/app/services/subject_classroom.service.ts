f3586889f1cdd480b058103467514bc8ad307b73
jjeanjacques10 <jjean.jacques10@gmail.com>
2021-09-20 21:24:14 -0300

    add throw error

--- app/src/app/services/subject_classroom.service.ts
+++ app/src/app/services/subject_classroom.service.ts
@@ -11,6 +11,10 @@ class SubjectClassroomService {
         this.repository = connection.getRepository(SubjectClassroom)
 
         const subjectClassroom = await this.repository.find({})
+
+        if (!subjectClassroom) {
+            throw new Error("Subject Classroom not found")
+        }
         return subjectClassroom
     }
 
@@ -32,6 +36,10 @@ class SubjectClassroomService {
             where: { teachers_id: teacherId },
             relations: ["classroom", "school_subject", "school", "teacher"]
         })
+
+        if (!subjectClassroom) {
+            throw new Error("Subject Classroom not found")
+        }
         return subjectClassroom
     }
 
